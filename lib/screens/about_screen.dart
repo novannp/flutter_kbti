d28c50abce5014cf5026623bc0ad8c30a85f8132
novannp <novangarut@gmail.com>
2022-07-02 22:20:52 +0700

    optimize about screen ui

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -25,74 +25,93 @@ class AboutScreen extends StatelessWidget {
         child: ListView(
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 10),
-                  Text(
-                    'Tentang Aplikasi',
-                    style: GoogleFonts.lato(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      color: blueColor,
-                    ),
-                  ),
-                  const Divider(),
-                  const SizedBox(height: 10),
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: 50,
-                  ),
-                  const Text(
-                    '2022 \u00a9 KBTI',
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'KBTI atau Kamus Besar Teknologi Informasi adalah website yang menyediakan istilah dan definisi dalam dunia Informasi Teknologi yang ditulis berdasarkan partisipasi pengguna',
-                    textAlign: TextAlign.justify,
-                    style: GoogleFonts.lato(
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Tim Pengembang',
-                    style: GoogleFonts.lato(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      color: blueColor,
-                    ),
-                  ),
-                  const Divider(),
-                  const SizedBox(height: 10),
-                  Wrap(
-                    spacing: 20,
-                    runSpacing: 20,
-                    children: const [
-                      AvatarDeveloper(
-                          name: 'Zahra Aulia Rahmadianti',
-                          networkImage: 'https://picsum.photos/200',
-                          collage: 'Universitas Brawijaya'),
-                      AvatarDeveloper(
-                          name: 'Mochamad Rizky Cahya Diputra',
-                          networkImage: 'https://picsum.photos/300',
-                          collage: 'Institut Teknologi Garut'),
-                      AvatarDeveloper(
-                          name: 'Tryo Asnafi',
-                          networkImage: 'https://picsum.photos/400',
-                          collage: 'Politeknik Negeri Bengkalis'),
-                      AvatarDeveloper(
-                          name: 'Sofyan Egi Lesmana',
-                          networkImage: 'https://picsum.photos/500',
-                          collage: 'Universitas  Pasundan'),
-                      AvatarDeveloper(
-                          name: 'Novan Noviansyah Pratama',
-                          networkImage: 'https://picsum.photos/600',
-                          collage: 'Institut Teknologi Garut'),
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      Text(
+                        'Tentang Aplikasi',
+                        style: GoogleFonts.lato(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: blueColor,
+                        ),
+                      ),
+                      const Divider(),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        'assets/images/logo.png',
+                        height: 50,
+                      ),
+                      const Text(
+                        '2022 \u00a9 KBTI',
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'KBTI atau Kamus Besar Teknologi Informasi adalah website yang menyediakan istilah dan definisi dalam dunia Informasi Teknologi yang ditulis berdasarkan partisipasi pengguna',
+                        textAlign: TextAlign.justify,
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                        ),
+                      ),
                     ],
-                  )
-                ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Text(
+                        'Tim Pengembang',
+                        style: GoogleFonts.lato(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: blueColor,
+                        ),
+                      ),
+                      const Divider(),
+                      const SizedBox(height: 10),
+                      Wrap(
+                        spacing: 20,
+                        runSpacing: 20,
+                        children: const [
+                          AvatarDeveloper(
+                              name: 'Zahra Aulia Rahmadianti',
+                              networkImage: 'https://picsum.photos/200',
+                              collage: 'Universitas Brawijaya'),
+                          AvatarDeveloper(
+                              name: 'Mochamad Rizky Cahya Diputra',
+                              networkImage: 'https://picsum.photos/300',
+                              collage: 'Institut Teknologi Garut'),
+                          AvatarDeveloper(
+                              name: 'Tryo Asnafi',
+                              networkImage: 'https://picsum.photos/400',
+                              collage: 'Politeknik Negeri Bengkalis'),
+                          AvatarDeveloper(
+                              name: 'Sofyan Egi Lesmana',
+                              networkImage: 'https://picsum.photos/500',
+                              collage: 'Universitas  Pasundan'),
+                          AvatarDeveloper(
+                              name: 'Novan Noviansyah Pratama',
+                              networkImage: 'https://picsum.photos/600',
+                              collage: 'Institut Teknologi Garut'),
+                        ],
+                      ),
+                      const SizedBox(height: 14)
+                    ],
+                  ),
+                ),
               ),
             )
           ],
